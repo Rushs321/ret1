d00b05fa508e5b8da4f097478b645cb69c22a938
Rushs321 <fyggh566@tutamail.com>
2024-09-08 09:46:54 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -40,11 +40,14 @@ export async function processRequest(request, reply) {
         return reply.send(`bandwidth-hero-proxy`);
     }
 
+    if (Array.isArray(url)) url = url.join('&url=');
+    url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 
-    request.params.url = decodeURIComponent(url);
+
+    request.params.url = url;
     request.params.webp = !request.query.jpeg;
     request.params.grayscale = request.query.bw != '0';
-    request.params.quality = parseInt(request.query.l, 10) || 40;
+    request.params.quality = parseInt(request.query.l, 10) || 1;
 
     
 
